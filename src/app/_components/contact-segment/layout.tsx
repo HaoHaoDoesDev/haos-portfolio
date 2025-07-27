b78df850f8f913afe6cc8538fd2c6550a0ed29e8
JunHaoOng <junhao@ambercreative.sg>
2025-07-28 00:25:32 +0800

    faet: fixed issues with deployment

---
+++ src/app/_components/contact-segment/layout.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { Button } from "@/components/ui/button";
+
+export default function ContactContent() {
+  return (
+    <main className="relative w-full mb-8 pt-8 mt-8">
+      <div className="flex text-white text-bold justify-center font-funnel max-w-xl mx-auto text-4xl ">
+        Contact Me
+      </div>
+    </main>
+  );
+}
